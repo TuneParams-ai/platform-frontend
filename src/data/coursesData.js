483cde671f51f8f4a5b7867551333376ff45ee37
Abhinaykotla <abhinaykotla@gmail.com>
2025-07-12 23:49:45 -0500

    Test

--- src/data/coursesData.js
+++ src/data/coursesData.js
@@ -13,7 +13,7 @@ export const coursesData = [
         maxCapacity: 30,
         nextBatchDate: "2025-08-1",
         rating: 5,
-        price: 299,
+        price: 0,
         originalPrice: 899,
         icon: "🤖",
         image: "/data/FAAI/FAAI_thumbnail.png",
